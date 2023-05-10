712fa7787e920c414e45a3127ec8035a257372ac
Natali <114168847+Natali9108@users.noreply.github.com>
2023-05-11 02:22:50 +0300

    Update Api.js

--- src/servises/Api.js
+++ src/servises/Api.js
@@ -1,16 +1,21 @@
 import axios from 'axios';
 
 export const fetchImagesWithQuery = async searchQuery => {
-  const response = await axios.get('https://pixabay.com/api/', {
-    params: {
-      key: '34816104-0e2476e874eadf366edbb741b',
-      q: `${searchQuery}`,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      per_page: 12,
-    },
-  });
+  const response =
+    await axios.get(`https://pixabay.com/api/?key=34816104-0e2476e874eadf366edbb741b&q=${searchQuery}&page=1&image_type=photo&orientation=horizontal&per_page=12
 
+  `);
+  // {
+  //   params: {
+  //     key: '34816104-0e2476e874eadf366edbb741b',
+  //     q: `${searchQuery}`,
+  //     image_type: 'photo',
+  //     orientation: 'horizontal',
+  //     per_page: 12,
+  //   },
+  // });
+  // console.log(response.headers);
+  // console.log(response.config);
   return response.data.hits;
 };
 
